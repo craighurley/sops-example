87ef01593e6194a8fbd53d5b280880236432cf6e
Craig Hurley <craig@thehurley.com>
2023-05-12 19:52:54 +1200

    Remove un-encrypted files after encrypting them

--- encrypt.sh
+++ encrypt.sh
@@ -3,7 +3,10 @@
 set -e
 
 sops -e test.env > sops.test.env
+shred -z --remove test.env
 sops -e test.json > sops.test.json
+shred -z --remove test.json
 sops -e test.yaml > sops.test.yaml
+shred -z --remove test.yaml
 
 exit 0
